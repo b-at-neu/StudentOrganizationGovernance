6cf3b5876c4cc17497e385fa318d318994fd48f4
b-at-neu <winkler.b@northeastern.edu>
2024-04-18 22:02:44 +0100

    Adjusted environ vars

--- student_org_gov/student_org_gov/wsgi.py
+++ student_org_gov/student_org_gov/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_org_gov.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_org_gov.student_org_gov.settings')
 
 application = get_wsgi_application()
